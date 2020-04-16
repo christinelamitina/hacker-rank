70b7a5f5b45c1134153b221b1151430d9ca60973
Christine Lamitina <christinelamitina@gmail.com>
2020-04-16 09:46:13 -0500

    April 16

--- kataChallenges.js
+++ kataChallenges.js
@@ -3,3 +3,11 @@
 function check(a, x) {
     return a.includes(x);
   }
+
+//More than 3 characters but less than 20
+//Must contain only alphanumeric characters.
+//Must contain letters and numbers.
+
+function validPass(password){
+    return /(?=.+[a-z])(?=.+\d)^[a-z\d]{3,20}$/i.test(password) ? 'VALID' : 'INVALID';
+  }

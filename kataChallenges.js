7d54aa6d7be5f1e0bdadb97bd0609160a4bf6db2
Christine Lamitina <christinelamitina@gmail.com>
2020-05-21 18:51:07 -0500

    may 21

--- kataChallenges.js
+++ kataChallenges.js
@@ -57,3 +57,15 @@ function validPass(password){
     
     return name;
   }
+
+  // Complete the solution so that it splits the string into pairs of two characters. 
+  // If the string contains an odd number of characters then it should replace the missing
+  // second character of the final pair with an underscore ('_').
+
+  function solution(str) {
+    if (str.length == 0) {
+      return []
+    };
+    
+    return (str.length % 2 ? str + '_' : str).match(/../g);
+  }

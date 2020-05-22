80965ca9a7648f178e3fc37eeca211596ae8a90f
Christine Lamitina <christinelamitina@gmail.com>
2020-05-22 16:12:43 -0500

    May 22

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -66,3 +66,32 @@ function getSecondLargest(nums) {
     // Complete the function
     return nums.sort((a, b) => {return b - a;}).filter(el => {return nums[0] !== el})[0];
 }
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon {
+    constructor(heights) {
+        let polygon_perimeter = 0;
+        for (let index in heights) {
+            polygon_perimeter += heights[index];
+        }
+
+        this.my_perimeter = polygon_perimeter;
+    }
+
+    perimeter() {
+        return this.my_perimeter;
+    }
+
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
